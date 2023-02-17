a2f95a9c20ded7ac4945587deb34d4accf7bfebf
ttaehyun <rth0824@naver.com>
2023-02-17 22:49:41 +0900

    1day

--- src/demo_topic_subscriber.cpp
+++ src/demo_topic_subscriber.cpp
@@ -1,15 +1,18 @@
 #include "ros/ros.h"
 #include "std_msgs/Int32.h"
+#include "mastering_ros_demo_pkg/demo_msg.h"
 #include <iostream>
 
-void number_callback(const std_msgs::Int32::ConstPtr& msg) {
-    ROS_INFO("Received [%d]", msg->data);
+void number_callback(const mastering_ros_demo_pkg::demo_msg msg) {
+    ROS_INFO("Greeting, %s", msg.greeting.c_str());
+    ROS_INFO("Received [%d]", msg.number);
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv,"demo_topic_subscriber");
     ros::NodeHandle node_obj;
-    ros::Subscriber nubmer_subscriber = node_obj.subscribe("/numbers", 10, number_callback);
+    ros::Subscriber nubmer_subscriber = node_obj.subscribe("/msg", 10, number_callback);
     ros::spin();
     return 0;
+    
 }
